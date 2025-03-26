e4ddf235c161e52f21d383e5e8ad4cccb1f9da44
JRFACTORY <julian.rojas@controlonlineinternational.com>
2025-03-26 15:51:33 -0500

    Ajuste Card

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -5,17 +5,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const CardComponent = ({ icon, number, text, classcard, classicon}) => {
 
   return (
-    <div className="col-md-7 col-lg-4 mt-3 px-2">
+    <div className="col-12 col-sm-6 col-md-5 col-lg-6 mt-3 px-2">
       <div className={`card ${classcard}`}>
         <div className="row">
-          <div className="col-5 col-sm-5 col-md-4 d-flex justify-content-center align-items-center">
-            <div className={`icon-card ${classicon}`}>
+          <div className="col-4 d-flex justify-content-center align-items-center">
+          <div className={`icon-card ${classicon}`}>
               <FontAwesomeIcon icon={icon} />
             </div>
           </div>
-          <div className="col-7 col-sm-7 col-md-8 d-flex flex-column justify-content-center">
-            <h2 className="m-0 num-card">{number}</h2>
-            <p className="m-0 text-card">{text}</p>
+          <div className="col-8 d-flex flex-column justify-content-center">
+          <h2 className="m-0 num-card">{number}</h2>
+          <p className="m-0 text-card">{text}</p>
           </div>
         </div>
       </div>
